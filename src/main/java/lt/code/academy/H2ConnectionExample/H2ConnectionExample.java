7d6b544ed461fd2793f6c51bf8a4c234cff460fe
gamblerLt <rimas.spakauskas@outlook.com>
2023-02-20 21:01:48 +0200

    02-20-SQL-connection

--- src/main/java/lt/code/academy/H2ConnectionExample/H2ConnectionExample.java
+++ src/main/java/lt/code/academy/H2ConnectionExample/H2ConnectionExample.java
@@ -9,31 +9,49 @@ public class H2ConnectionExample {
 
     public static void main(String[] args) throws SQLException {
         //1. sukurti rysi -> butina mavene nurodyti dependencio draiverius
-        Connection connection = DriverManager.getConnection(URL,USERNAME, PASSWORD);
-        if(connection == null) {
+        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        if (connection == null) {
             System.out.println("Not connected!");
             return;
         }
         System.out.println("Connected!!!");
 
         //2. sukurti statementa
-       Statement statement = connection.createStatement();
+        Statement statement = connection.createStatement();
 
-       //3. execute statement
+        //3. execute statement
+
+        //statement.executeQuery naudojamas selektui
+        //statement.execute -> delete
+        //statement.executeUpdate --> atnaujinimui
 
         ResultSet resultSet = statement.executeQuery("SELECT * FROM DARBUOTOJAS");
+
         H2ConnectionExample example = new H2ConnectionExample();
         example.printEmployers(resultSet);
 
         //galima gauti databese info
         DatabaseMetaData metaData = connection.getMetaData();
-        ResultSet tablesResultSet =metaData.getTables(null, "PUBLIC", null, null);
+        ResultSet tablesResultSet = metaData.getTables(null, "PUBLIC", null, null);
         System.out.println("Visos lenteles, esancios duomenu bazeje:");
         while (tablesResultSet.next()) {
-           // System.out.println(tablesResultSet.getString(1));
-         //   System.out.println(tablesResultSet.getString(2));
+            // System.out.println(tablesResultSet.getString(1));
+            // System.out.println(tablesResultSet.getString(2));
             System.out.println(tablesResultSet.getString(3));
         }
+
+        //SELCT WITH FILTERS
+        System.out.println("\nIssfiltruoti useriai:");
+       // resultSet = statement.executeQuery("SELECT * FROM DARBUOTOJAS  WHERE PROJEKTAS_ID = 1 AND ALGA >= 3200");
+        int id = 1;
+        int sum = 300;
+       PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM DARBUOTOJAS  WHERE PROJEKTAS_ID = ? AND ALGA >= ?");
+       preparedStatement.setInt(1, id);
+       preparedStatement.setInt(2, sum);
+       resultSet = preparedStatement.executeQuery();
+
+
+        example.printEmployers(resultSet);
 
     }
 
